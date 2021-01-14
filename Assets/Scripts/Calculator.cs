4df24a535ee48377e8ac4686fed962bf95859e9f
Roman Oleksiichuk <47693169+Roksi921@users.noreply.github.com>
2021-01-14 17:03:44 +0200

    update1

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public Text Text1;
+    public string calc1;
+    public string x1;
+
+    public void On_Click_buttom()
+    {
+        Text1.text += calc1;
+    }
+
+    public void On_Click_C()
+    {
+        Text1.text = "";
+    }
+
+    public void On_Click_Ravno()
+    {
+        DataTable dt = new DataTable();
+        x1 = (dt.Compute(Text1.text, "")).ToString();
+        Text1.text = x1;
+    }
+}
